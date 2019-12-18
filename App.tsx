37b0bcf8e94c390368375ae66b87a0d2a931c74a
ivan <ivanpilot@gmail.com>
2019-12-18 20:48:32 +0100

    added configuration file such as linter and prettier

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    const [outputText, setText] = useState(
+        'Open up App.js to start working on your app',
+    );
+    return (
+        <View style={styles.container}>
+            <Text>{outputText}</Text>
+            <Button
+                title="Change Text"
+                onPress={() => setText('The text has change!!')}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        flex: 1,
+        justifyContent: 'center',
+    },
 });
