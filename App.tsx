e9be823e3f25e01cac0e9d8b1ae1f18e80ab9310
ivan <ivanpilot@gmail.com>
2019-12-20 18:40:18 +0100

    created navigation flow between nested flows and screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,27 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import AccountScreen from './src/components/AccountScreen';
 import SigninScreen from './src/components/SigninScreen';
 import SignupScreen from './src/components/SignupScreen';
 import TrackCreateScreen from './src/components/TrackCreateScreen';
 import TrackDetailScreen from './src/components/TrackDetailScreen';
 import TrackListScreen from './src/components/TrackListScreen';
+
+const switchNavigator = createSwitchNavigator({
+    loginFlow: createStackNavigator({
+        Signin: SigninScreen,
+        Signup: SignupScreen,
+    }),
+    mainFlow: createBottomTabNavigator({
+        Account: AccountScreen,
+        TrackCreate: TrackCreateScreen,
+        trackListFlow: createStackNavigator({
+            TrackDetail: TrackDetailScreen,
+            TrackList: TrackListScreen,
+        }),
+    }),
+});
+
+export default createAppContainer(switchNavigator);
