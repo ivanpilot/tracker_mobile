db56be40efe3e3d4326f277693b30503f4c4cb40
ivan <ivanpilot@gmail.com>
2019-12-23 08:35:47 +0100

    done signin screen

--- src/screens/SigninScreen.tsx
+++ src/screens/SigninScreen.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SigninScreen = ({ navigation }: any) => {
     const toSignup = () => {
         navigation.navigate('Signup');
     };
+    const { state, signin } = useContext(AuthContext);
 
     return (
-        <View>
-            <Text style={{ fontSize: 48 }}>SigninScreen</Text>
-            <Button title="Go to Signup" onPress={toSignup} />
+        <View style={styles.container}>
+            <AuthForm
+                headerText="Sign In to Your Account"
+                errorMessage={state.errorMessage}
+                onSubmit={signin}
+                buttonText="Sign In"
+            />
+            <NavLink
+                routeName="Signup"
+                text="Don't have an account? Signup instead"
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+    return {
+        header: null,
+    };
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 250,
+    },
+});
 
 export default SigninScreen;
