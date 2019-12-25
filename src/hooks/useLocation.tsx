9da98582011d3bd033badfa232518c4a713cec8b
ivan <ivanpilot@gmail.com>
2019-12-25 23:57:44 +0100

    first pass at turning on/off the user tracking location

--- src/hooks/useLocation.tsx
+++ src/hooks/useLocation.tsx
@@ -5,21 +5,28 @@ import {
 } from 'expo-location';
 import { useEffect, useState } from 'react';
 
-export default (callback: any) => {
+export default (shouldTrack: boolean, callback: any) => {
+    const initSub: any = null;
     const [err, setError] = useState(null);
 
-    useEffect(() => {
-        startWatching();
-    }, []);
+    /**
+     * The reason we create a state variable of subscriber is simply because we need to access it from useEffect while the sub variable is available only inside the startWatching function
+     */
+    const [subscriber, setSubscriber] = useState(initSub);
 
     const startWatching = async () => {
         try {
             await requestPermissionsAsync();
 
             /**
-             * each the user location changes, the callback is called
+             * once watchPositionAsync is called, the tracking features is activated and stays on. we specifically needs to manually removes it to stop tracking.
+             * We could have avoid to saved await watchPositionAsync in a variable but in that case we would not be able to remove the watchPosition to deactivate the geo location tracking
              */
-            await watchPositionAsync(
+
+            /**
+             * each time the user location changes, the callback is called
+             */
+            const sub = await watchPositionAsync(
                 {
                     accuracy: Accuracy.BestForNavigation,
                     distanceInterval: 10,
@@ -27,10 +34,27 @@ export default (callback: any) => {
                 },
                 callback,
             );
+            setSubscriber(sub);
         } catch (e) {
             setError(e);
         }
     };
+
+    /**
+     * useEffect is called each time our hook is called and useEffect is called each time the TrackCreateScreen gets re-rendered.
+     * Every time this hook is executed, react will compared the new value of shouldTrack with its previous value and if the value is different then it will run the callback inside useEffect
+     */
+    useEffect(() => {
+        if (shouldTrack) {
+            startWatching();
+        } else {
+            subscriber.remove();
+            /**
+             * Once remove, we don't need a subscriber anymore so we rest it to null
+             */
+            setSubscriber(null);
+        }
+    }, [shouldTrack]);
 
     /**
      * By convention, hooks return value through an array.
--- src/screens/TrackCreateScreen.tsx
+++ src/screens/TrackCreateScreen.tsx
@@ -1,17 +1,21 @@
 import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
-import { SafeAreaView } from 'react-navigation';
+import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 // import '../_mockLocation';
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
 
-const TrackCreateScreen = () => {
+/**
+ * withNavigationFocus is a HOC that provides additional props to elements it wraps.
+ * Once TrackCreateScreen is wrapped by withNavigationFocus, we can access isFocused props that will help up determinate when a user is not focusing on a screen and as such, in this example, stop the geotracking that eats battery
+ */
+const TrackCreateScreen = ({ isFocused }: any) => {
     const { addLocation } = useContext(LocationContext);
 
     // const [err] = useLocation((location: any) => addLocation(location));
-    const [err] = useLocation(addLocation);
+    const [err] = useLocation(isFocused, addLocation);
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h3={true}>Create a track</Text>
@@ -23,4 +27,4 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
